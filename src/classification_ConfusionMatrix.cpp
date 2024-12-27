9a7dbc56ae92966167b95e75af13e69748a73c41
serkor1 <77464572+serkor1@users.noreply.github.com>
2024-12-27 16:59:48 +0100

    New Matrix Class :hammer:
    * The matrix class reduces the amount of repeated code.
    * The matrix templates have been incorporated in the class, and are using overloading instead of if-statements
    * Updated NAMESPACE, and associated methods.

--- src/classification_ConfusionMatrix.cpp
+++ src/classification_ConfusionMatrix.cpp
@@ -8,37 +8,18 @@ using namespace Rcpp;
 //' @method cmatrix factor
 //' @export
 // [[Rcpp::export(cmatrix.factor)]]
-Rcpp::NumericMatrix ConfusionMatrix(const Rcpp::IntegerVector& actual, const Rcpp::IntegerVector& predicted, const Rcpp::Nullable<Rcpp::NumericVector>& w = R_NilValue) 
+Rcpp::NumericMatrix UnweightedConfusionMatrix(const Rcpp::IntegerVector& actual, const Rcpp::IntegerVector& predicted) 
 {
+    ConfusionMatrixClass args(actual, predicted);
+    return args.constructMatrix();
+}
 
-    // 1) declare the output matrix
-    // compiler doesn't recognize it in if-statements
-    Rcpp::NumericMatrix output;
-
-    // 1.1) extract levels (classes)
-    // from the actual-vector and the length
-    Rcpp::CharacterVector levels = actual.attr("levels");
-    int k = levels.length() + 1;
-
-    // 2) Determine the weights
-    // these are what determines 
-    // the content of the confusion matrix
-     if (w.isNull()) {
-        
-        output = Rcpp::wrap(confusionMatrix<Eigen::MatrixXd>(actual, predicted, k));
-
-    } else {
-        
-        output = Rcpp::wrap(confusionMatrix<Eigen::MatrixXd>(actual, predicted, k, w));
-
-    }
-
-    // 3) preare the matrix output
-    // by adding column and rownames
-    // and classes
-    Rcpp::rownames(output) = levels;
-    Rcpp::colnames(output) = levels;
-    output.attr("class") = "cmatrix";
-
-    return output;
+//' @rdname cmatrix
+//' @method weighted.cmatrix factor
+//' @export
+// [[Rcpp::export(weighted.cmatrix.factor)]]
+Rcpp::NumericMatrix WeightedConfusionMatrix(const Rcpp::IntegerVector& actual, const Rcpp::IntegerVector& predicted, const Rcpp::NumericVector& w) 
+{
+    ConfusionMatrixClass args(actual, predicted);
+    return args.constructMatrix(w);
 }
