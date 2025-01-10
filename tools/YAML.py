6d678534b85126dac9c4ae116d1b8f50477e17d8
serkor1 <77464572+serkor1@users.noreply.github.com>
2025-01-10 18:22:11 +0100

    :books: Streamlined docs
    * Set common font and fontsize
    * Fixed a bug in parse_rd where it would aggresively replace < and >

--- tools/YAML.py
+++ tools/YAML.py
@@ -45,9 +45,11 @@ book_structure = {
     'format': {
         'html': {
             'theme': {
-            "light": ["cosmo", "theme-light.scss"],  # Use a list here
-            "dark": ["slate", "theme-dark.scss"],  # Use a list here
-        }
+            "light": ["cosmo", "theme-light.scss"],
+            "dark": ["slate", "theme-dark.scss"]
+        },
+            "fontsize": "18px",
+            "mainfont": "calibri"
         },
         'pdf': {'documentclass': 'scrreprt'}
     },
@@ -72,9 +74,6 @@ try:
 
 except:
     print("No references found! Skipping")
-
-
-
 
 with open('_quarto.yml', 'w') as file:
     yaml.dump(book_structure, file, sort_keys=False)
