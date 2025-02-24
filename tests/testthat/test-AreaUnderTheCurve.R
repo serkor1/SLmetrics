b2d4f855912ab38d74dba74b5838df2a891663ab
serkor1 <77464572+serkor1@users.noreply.github.com>
2025-02-24 12:49:37 +0100

    :hammer: Updated Unit-tests
    * The unit-tests now includes weighted auc for precision-recall and ROC

--- tests/testthat/test-AreaUnderTheCurve.R
+++ tests/testthat/test-AreaUnderTheCurve.R
@@ -39,82 +39,104 @@ testthat::test_that(
 
     }
 
-    for (ordered in c(TRUE, FALSE)) {
+    for (weighted in c(TRUE, FALSE)) {
 
-      # 1) actual values
-      # and response variables
-      actual   <- create_factor()
-      response <- runif(length(actual))
-      w        <- runif(length(actual))
+      for (ordered in c(TRUE, FALSE)) {
 
-      if (ordered) {
-        # 1.1) create order
-        # by response
-        idx      <- order(response, decreasing = TRUE)
-        actual   <- actual[idx]
-        response <- response[idx]
-        w        <- w[idx]
+        # 1) actual values
+        # and response variables
+        actual   <- create_factor()
+        response <- runif(length(actual))
+        w        <- runif(length(actual))
+  
+        if (ordered) {
+          # 1.1) create order
+          # by response
+          idx      <- order(response, decreasing = TRUE)
+          actual   <- actual[idx]
+          response <- response[idx]
+          w        <- w[idx]
+  
+        }
+  
+        # 1.2) calculate ROC object
+        # and store
+        if (weighted) {
 
-      }
+          object <- weighted.ROC(
+            actual   = actual,
+            response = response,
+            w        = w
+          )
 
-      # 1.2) calculate ROC object
-      # and store
-      object <- ROC(
-        actual   = actual,
-        response = response
-      )
+        } else {
 
-      # 2) calculate AUC
-      # based on methods
-      for (method in c(0, 1)) {
+          object <- ROC(
+            actual   = actual,
+            response = response
+          )
 
-        label <- paste(
-          "Method  = ", method,
-          "Ordered = ", ordered
-        )
 
-        # 1.1) extract list
-        # of labels
-        x_list <- split(
-          x = object,
-          f = object$label
-        )
-
-        # 1.2) calculate AUC
-        # for each label
-        reference <- vapply(
-          x_list, 
-          function(x) {
-            auc(
-              y = x$tpr,
-              x = x$fpr,
-              method = method
-            )
-          }, 
-          FUN.VALUE = numeric(1),
-          USE.NAMES = TRUE
-        )
-
-        names(reference) <- names(x_list)
-
-        # 2.2) check for
-        # equality
-        testthat::expect_true(
-          object = set_equal(
-            reference,
-            roc_auc(
-              actual   = actual,
-              response = response,
-              method   = method,
-              ordered  = ordered
-            )
-          ),
-          info = label
-        )
+        }
         
-      
+  
+        # 2) calculate AUC
+        # based on methods
+        for (method in c(0, 1)) {
+  
+          label <- paste(
+            "Method   =", method,
+            "Ordered  =", ordered,
+            "Weighted =", weighted
+          )
+  
+          # 1.1) extract list
+          # of labels
+          x_list <- split(
+            x = object,
+            f = object$label
+          )
+  
+          # 1.2) calculate AUC
+          # for each label
+          reference <- vapply(
+            x_list, 
+            function(x) {
+              auc(
+                y = x$tpr,
+                x = x$fpr,
+                method = method
+              )
+            }, 
+            FUN.VALUE = numeric(1),
+            USE.NAMES = TRUE
+          )
+  
+          names(reference) <- names(x_list)
+  
+          # 2.2) check for
+          # equality
+          testthat::expect_true(
+            object = set_equal(
+              reference,
+              roc_auc(
+                actual   = actual,
+                response = response,
+                method   = method,
+                ordered  = ordered,
+                w        = if (weighted) w else NULL
+              )
+            ),
+            info = label
+          )
+          
+        
+        }
       }
+      
     }
+
+    
   }
 )
 
@@ -124,7 +146,7 @@ testthat::test_that(
     # 0) skip on CRAN
     testthat::skip_on_cran()
 
-    # 1) create roc.auc()-wrapper
+    # 1) create pr.auc()-wrapper
     pr_auc <- function(
       actual, 
       response, 
@@ -155,81 +177,101 @@ testthat::test_that(
 
     }
 
-    for (ordered in c(TRUE, FALSE)) {
+    for (weighted in c(TRUE, FALSE)) {
 
-      # 1) actual values
-      # and response variables
-      actual   <- create_factor(k = 2)
-      response <- runif(length(actual))
-      w        <- runif(length(actual))
+      for (ordered in c(TRUE, FALSE)) {
 
-      if (ordered) {
-        # 1.1) create order
-        # by response
-        idx      <- order(response,decreasing = TRUE)
-        actual   <- actual[idx]
-        response <- response[idx]
-        w        <- w[idx]
+        # 1) actual values
+        # and response variables
+        actual   <- create_factor(k = 2)
+        response <- runif(length(actual))
+        w        <- runif(length(actual))
+  
+        if (ordered) {
+          # 1.1) create order
+          # by response
+          idx      <- order(response,decreasing = TRUE)
+          actual   <- actual[idx]
+          response <- response[idx]
+          w        <- w[idx]
+  
+        }
+  
+        # 1.2) calculate precision-recall
+        # object
+        if (weighted) {
 
-      }
+          object <- weighted.prROC(
+            actual   = actual,
+            response = response,
+            w        = w
+          )
 
-      # 1.2) calculate precision-recall
-      # object
-      object <- prROC(
-        actual   = actual,
-        response = response
-      )
+        } else {
 
-      # 2) calculate AUC
-      # based on methods
-      for (method in c(0, 1)) {
+          object <- prROC(
+            actual   = actual,
+            response = response
+          )
 
-        label <- paste(
-          "Method  = ", method,
-          "Ordered = ", ordered
-        )
-
-        # 1.1) extract list
-        # of labels
-        x_list <- split(
-          x = object,
-          f = object$label
-        )
-
-        # 1.2) calculate AUC
-        # for each label
-        reference <- vapply(
-          x_list, 
-          function(x) {
-            auc(
-              y = x$precision,
-              x = x$recall,
-              method = method
-            )
-          }, 
-          FUN.VALUE = numeric(1),
-          USE.NAMES = TRUE
-        )
-
-        names(reference) <- names(x_list)
-
-        # 2.2) check for
-        # equality
-        testthat::expect_true(
-          object = set_equal(
-            reference,
-            pr_auc(
-              actual   = actual,
-              response = response,
-              method   = method,
-              ordered  = ordered
-            )
-          ),
-          info = label
-        )
+        }
         
-      
+  
+        # 2) calculate AUC
+        # based on methods
+        for (method in c(0, 1)) {
+  
+          label <- paste(
+            "Method  = ", method,
+            "Ordered = ", ordered,
+            "Weighted=", weighted
+          )
+  
+          # 1.1) extract list
+          # of labels
+          x_list <- split(
+            x = object,
+            f = object$label
+          )
+  
+          # 1.2) calculate AUC
+          # for each label
+          reference <- vapply(
+            x_list, 
+            function(x) {
+              auc(
+                y = x$precision,
+                x = x$recall,
+                method = method
+              )
+            }, 
+            FUN.VALUE = numeric(1),
+            USE.NAMES = TRUE
+          )
+  
+          names(reference) <- names(x_list)
+  
+          # 2.2) check for
+          # equality
+          testthat::expect_true(
+            object = set_equal(
+              reference,
+              pr_auc(
+                actual   = actual,
+                response = response,
+                method   = method,
+                ordered  = ordered,
+                w        = if (weighted) w else NULL
+              )
+            ),
+            info = label
+          )
+          
+        
+        }
       }
+
     }
+
   }
 )
