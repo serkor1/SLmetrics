a2790c8a1ee7e50ecd27a24bd22f5070011dab57
serkor1 <77464572+serkor1@users.noreply.github.com>
2025-04-08 21:20:11 +0200

    🔥 Ported Recall to Armadillo

--- src/classification_Recall.h
+++ src/classification_Recall.h
@@ -1,69 +1,48 @@
 #ifndef CLASSIFICATION_RECALL_H
 #define CLASSIFICATION_RECALL_H
 
-#include "classification_Helpers.h"
-#include <RcppEigen.h>
-#include <cmath>
-#define EIGEN_USE_MKL_ALL
-EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+#include "SLmetrics.h"
+#include <armadillo>
 
-/*
-    NOTE:
-        To increase maintainability all functions are passed through
-        the confusion matrix. So there is no need to add a overloaded function
-        for the weighted metrics.
-*/
-class RecallClass : public classification {
+namespace metric {
 
-    private:
-        bool na_rm;
+    template <typename T>
+    using base_metric = classification::metric_tools::base_metric<T>;
 
+    using aggregate = classification::metric_tools::aggregation_level;
+    
+    template <typename T>
+    class recall : public base_metric<T> {
+    protected:
+        arma::Col<double> calculate_class_values() const override {
+            return this->tp_ / (this->tp_ + this->fn_);
+        }
+        
+        double calculate_micro_value() const override {
+            return this->calculate_micro([](double tp, double fp, double fn, double tn) {
+                return tp / (tp + fn);
+            });
+        }
+        
     public:
+        recall(const vctr_t<T>& actual, 
+               const vctr_t<T>& predicted, 
+               aggregate mode = aggregate::CLASS_WISE, 
+               bool na_rm = true) 
+               : base_metric<T>(actual, predicted, mode, na_rm) {}
 
-        RecallClass(bool na_rm)
-            : na_rm(na_rm) {}
+        recall(const vctr_t<T>& actual, 
+               const vctr_t<T>& predicted,
+               const vctr_t<double>& weights, 
+               aggregate mode = aggregate::CLASS_WISE, 
+               bool na_rm = true) 
+               : base_metric<T>(actual, predicted, weights, mode, na_rm) {}
 
-        // Compute recall with micro or macro aggregation
-        Rcpp::NumericVector compute(const Eigen::MatrixXd& matrix, bool do_micro) const override {
-            
-            // 0) Declare variables and size
-            // for efficiency.
-            // NOTE: Micro and macro already wraps and exports as Rcpp
-            Rcpp::NumericVector output(1);
-            Eigen::ArrayXd tp(matrix.rows()), fn(matrix.rows());
+        recall(const Rcpp::NumericMatrix& x, 
+               aggregate mode = aggregate::CLASS_WISE, 
+               bool na_rm = true)
+               : base_metric<T>(x, mode, na_rm) {}
+    };
+}
 
-            // 1) create TP and FP arrays
-            // for calculations
-            TP(matrix, tp);
-            FN(matrix, fn);
-
-            return do_micro
-                ?  micro(tp, tp + fn, na_rm)
-                :  macro(tp, tp + fn, na_rm);
-
-        }
-
-        // Compute recall without micro aggregation
-        Rcpp::NumericVector compute(const Eigen::MatrixXd& matrix) const override {
-            
-            // 0) declare the
-            // output value and 
-            // TP/FP
-            Eigen::ArrayXd output(matrix.rows());
-            Eigen::ArrayXd tp(matrix.rows()), fn(matrix.rows());
-
-            // 1) create TP and FP arrays
-            // for calculations
-            TP(matrix, tp);
-            FN(matrix, fn); 
-
-            // 2) calculate metric
-            output = tp / (tp + fn);
-
-            // 3) rerturn with 
-            // wrap (R compatible classes)   
-            return Rcpp::wrap(output);
-        }
-};
-
-#endif // CLASSIFICATION_RECALL_H
+#endif
