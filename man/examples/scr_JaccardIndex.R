60b8b88b50ba1e28f97b047fd4b00c7583cdc434
Serkan Korkmaz <77464572+serkor1@users.noreply.github.com>
2025-04-09 21:44:58 +0200

    Update scr_JaccardIndex.R
    Signed-off-by: Serkan Korkmaz <77464572+serkor1@users.noreply.github.com>

--- man/examples/scr_JaccardIndex.R
+++ man/examples/scr_JaccardIndex.R
@@ -55,13 +55,13 @@ cat(
   "Micro-averaged Jaccard Index", jaccard(
     actual    = actual,
     predicted = predicted,
-    estiamtor = 1
+    estimator = 1
   ),
   "Micro-averaged Jaccard Index (weighted)", weighted.jaccard(
     actual    = actual,
     predicted = predicted,
     w         = iris$Petal.Length/mean(iris$Petal.Length),
-    estiamtor = 1
+    estimator = 1
   ),
   sep = "\n"
 )
