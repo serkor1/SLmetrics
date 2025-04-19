7511340e7c664ccd830c62c83887ca1e6ea93ef0
serkor1 <77464572+serkor1@users.noreply.github.com>
2025-04-19 17:06:35 +0200

    Deleted unused files

--- .meta/scripts/new_version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Script to create a new version entry in NEWS
-
-# Get the version from command line arguments
-VERSION=$1
-
-# Run the changelog template script
-./.meta/DEVTOOLS/changelog_template.sh
