dabbc259cd23a22801c483559ecaefb0b623f096
serkor1 <77464572+serkor1@users.noreply.github.com>
2025-05-01 17:07:07 +0200

    :hammer: Micro optimization of `gmse()`
    * In theory using __restrict__ and noexcept should enable aggresive compiler optimization - but the local tests suggests that this is no different than the original implementation. This is most likely due to compiler flags, but I really do not know at this point. The current implementation of the optimizations is not in harmony with OpenMP, so the option is either to revert to index based operations, or using #ifdef clauses (or whatever its called). Time will tell which implementation is best.

--- src/regression_GeometricMeanSquaredError.h
+++ src/regression_GeometricMeanSquaredError.h
@@ -2,48 +2,59 @@
 #define REGRESSION_GEOMETRICMEANSQUAREDERROR_H
 
 #include "SLmetrics.h"
-#include "utilities_Package.h"
 #include <cmath>
 #include <cstddef>
 
-#ifdef _OPENMP
-    #include <omp.h>
-#endif
-
 namespace metric {
-    /// Geometric Mean Squared Error (GMSE)
+    // Geometric Mean Squared Error (GMSE)
     template <typename T>
     class gmse : public regression::task<T> {
       public:
         using regression::task<T>::task;
 
-        inline T compute() const override {
-            arma::uword n = this -> actual_.n_elem;
+        inline T compute() const noexcept override {
+
+            // pointers and size
+            const arma::uword n_obs             = this -> actual_.n_elem;
+            const T* __restrict__ actual_ptr    = this -> actual_.memptr();
+            const T* __restrict__ predicted_ptr = this -> predicted_.memptr();
+
             T sum_log = 0;
-            for (arma::uword i = 0; i < n; ++i) {
-                T err = this -> actual_[i] - this -> predicted_[i];
-                sum_log += std::log(err * err);
+            const T* __restrict__ end = actual_ptr + n_obs;
+            for (; actual_ptr < ( end ); ++actual_ptr, ++predicted_ptr) {
+                sum_log += std::log(
+                     (*actual_ptr - *predicted_ptr) * ( *actual_ptr - *predicted_ptr ) 
+                );
             }
             
-            return std::exp(sum_log / static_cast<T>(n));
+            return std::exp( sum_log / static_cast<T>( n_obs ) );
         }
     };
 
-    /// Weighted Geometric Mean Squared Error
+    // Weighted Geometric Mean Squared Error
     template <typename T>
     class weighted_gmse : public regression::task<T> {
       public:
         using regression::task<T>::task;
 
-        inline T compute() const override {
-            arma::uword n = this -> actual_.n_elem;
-            T sum_wlog = 0, sum_w = 0;
-            for (arma::uword i = 0; i < n; ++i) {
-                T err = this -> actual_[i] - this -> predicted_[i];
-                sum_wlog += this -> weights_[i] * std::log(err * err);
-                sum_w   += this -> weights_[i];
+        inline T compute() const noexcept override {
+
+            // pointers and size
+            const arma::uword n_obs             = this -> actual_.n_elem;
+            const T* __restrict__ actual_ptr    = this -> actual_.memptr();
+            const T* __restrict__ predicted_ptr = this -> predicted_.memptr();
+            const T* __restrict__ weights_ptr   = this -> weights_.memptr();
+
+            T wsum_log = 0, sum_weights = 0;
+            const T* __restrict__ end = actual_ptr + n_obs;
+            for (; actual_ptr < ( end ); ++actual_ptr, ++predicted_ptr, ++weights_ptr) {
+                wsum_log     += *weights_ptr * std::log( 
+                    (*actual_ptr - *predicted_ptr) * (*actual_ptr - *predicted_ptr)
+                );
+                sum_weights  += *weights_ptr;
             }
-            return std::exp(sum_wlog / sum_w);
+
+            return std::exp( wsum_log / sum_weights );
         }
     };
 }
