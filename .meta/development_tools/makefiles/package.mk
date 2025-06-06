fc2166887b9570bd09a84c953f76bb17bf8713ae
serkor1 <77464572+serkor1@users.noreply.github.com>
2025-06-06 20:09:16 +0200

    :bug:-fix in Makefiles
    * The new commands werent embedded correctly. There is probably more bugs.
        - But who cares.

--- .meta/development_tools/makefiles/package.mk
+++ .meta/development_tools/makefiles/package.mk
@@ -5,15 +5,15 @@ pkg-benchmark: ## Run package benchmarks
 	@echo $(echo_success) "Performance tests completed successfully"
 	@echo $(echo_warning) "Remember to rebuild $(PKGNAME) to use the new data!"
 
-pkg-build: document ## Build and install the package
+pkg-build: docs-document ## Build and install the package
 	@echo $(echo_prefix) "Starting build process of $(PKGNAME)"
 	@$(scripts_directory)/build_package.sh $(CHECK)
 	@echo $(echo_success) "Build process completed"
 
 pkg-check: ## Run R CMD CHECK
-	@$(MAKE) build CHECK=true
+	@$(MAKE) pkg-build CHECK=true
 
-pkg-test: document ## Unit-tests with {testthat}
+pkg-test: docs-document ## Unit-tests with {testthat}
 	@echo $(echo_prefix) "Running unit tests"
 	@$(scripts_directory)/run_tests.sh
 	@echo $(echo_success) "Unit tests completed"
